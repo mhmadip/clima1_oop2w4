d32dec6977965b5368f2e088cef7c9e2f0da8629
Mohammad Salim <mhmadip@gmail.com>
2022-03-01 22:36:30 +0300

    Update networking.dart

---
+++ lib/services/networking.dart
@@ -0,0 +1,12 @@
+import 'package:http/http.dart';
+
+class NetworkHelper {
+  String url;
+  NetworkHelper(this.url);
+
+  Future<String> getData() async {
+    Response response = await get(Uri.parse(url));
+    print(response.statusCode);
+    return response.body;
+  }
+}
