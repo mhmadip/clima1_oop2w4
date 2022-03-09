4e3fb2ad005b64516b6ef6ff0a9afb88abed5880
Mohammad Salim <mhmadip@gmail.com>
2022-03-09 03:53:30 +0300

    Update networking.dart
    Adding jsonDecode function to convert String response data to JSON data

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,12 +1,14 @@
+import 'dart:convert';
+
 import 'package:http/http.dart';
 
 class NetworkHelper {
   String url;
   NetworkHelper(this.url);
 
-  Future<String> getData() async {
+  Future getData() async {
     Response response = await get(Uri.parse(url));
     print(response.body);
-    return response.body;
+    return jsonDecode(response.body);
   }
 }
